a131523c73ba299c503e7e3c1c91b64a47d64647
vismay25 <vishmaypatel2573@gmail.com>
2024-01-18 18:41:44 +0530

    day12

---
+++ DAY-12/day12/src/App.js
@@ -0,0 +1,18 @@
+// import Pic from './Components/Pic';
+import './App.css';
+import Form from './Components/Form';
+
+function App() {
+  return (
+    <div>
+        <div>
+          <Form />
+        </div><br />
+        {/* <div> */}
+          {/* <Pic /> */}
+        {/* </div> */}
+    </div>
+  );
+}
+
+export default App;
