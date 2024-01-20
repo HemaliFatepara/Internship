cc228bf99269d7a375e9bf34fe7f2924947ef7d5
vismay25 <vishmaypatel2573@gmail.com>
2024-01-20 18:52:10 +0530

    day14

---
+++ DAY-14/day14/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Form from './Components/Form'; 
+import Timer from './Components/Timer';
+
+function App() {
+  return (
+    <div >
+      <div>
+          <Form />
+      </div>
+      <div>
+          <Timer />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ DAY-14/day14/src/Components/Form.js
@@ -0,0 +1,49 @@
+import { useState } from 'react';
+
+export default function Form() {
+  const [showForm, setShowForm] = useState(true);
+  const [message, setMessage] = useState('');
+
+
+  function handleSubmit(e) {
+    
+        if (!showForm) {
+      e.preventDefault();
+      sendMessage(message);
+    } else {
+      setShowForm(false);
+    }
+
+  }
+
+  if (!showForm) {
+    return (
+      <>
+        <h1>Thanks for using our services!</h1>
+        <button onClick={() => {
+          setMessage('');
+          setShowForm(true);
+        }}>
+          Open chat
+        </button>
+      </>
+    );
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <textarea
+        placeholder="Message"
+        value={message}
+        onChange={e => setMessage(e.target.value)}
+      /><br />
+      <button type="submit" disabled={message === ''}>
+        Send
+      </button>
+    </form>
+  );
+}
+
+function sendMessage(message) {
+  console.log('Sending message: ' + message);
+}
