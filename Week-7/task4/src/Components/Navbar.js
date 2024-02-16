4222efbc28ffe5df642b63fe4096711037a5bcdb
vismay25 <vishmaypatel2573@gmail.com>
2024-02-16 18:04:11 +0530

    .

--- Week-7/task4/src/Components/Navbar.js
+++ Week-7/task4/src/Components/Navbar.js
@@ -190,6 +190,60 @@ const Navbar = () => {
                 type="button"
                 onClick={toggleMenu}
                 className="relative inline-flex items-center justify-center rounded-md p-2 text-black"
+                
+                aria-controls="mobile-menu"
+                aria-expanded={menuOpen ? "true" : "false"}
+              >
+                {/* Menu icon */}
+              </button>
+            </div>
+          </div>
+        </div>
+        <div
+          className={`${menuOpen ? "block" : "hidden"} sm:hidden shadow-lg`}
+          id="mobile-menu"
+        >
+          <div className="space-y-1 px-2 pb-3 pt-2 text-center">
+            <div>
+              <Link
+                to="/register"
+                className="text-black hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium"
+              >
+                Register
+              </Link>
+            </div>
+          </div>
+        </div>
+      </nav>
+    );
+  } else {
+    return (
+      <nav className="shadow-xl">
+        <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+          <div className="relative flex h-16 items-center justify-between">
+            <div className="flex items-center">
+              <div className="flex-shrink-0">
+                <FaUserDoctor className="text-2xl items-start" />
+              </div>
+              <Link to="/" className="px-2 text-xl">
+                <h3 className="text-xl">AppointCare</h3>
+              </Link>
+            </div>
+            <div className="hidden sm:flex space-x-4 ml-auto">
+              <div>
+                <Link
+                  to="/register"
+                  className="text-black hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
+                >
+                  Register
+                </Link>
+              </div>
+            </div>
+            <div className="absolute inset-y-0 right-0 flex items-center sm:hidden">
+              <button
+                type="button"
+                onClick={toggleMenu}
+                className="relative inline-flex items-center justify-center rounded-md p-2 text-black"
                 aria-controls="mobile-menu"
                 aria-expanded={menuOpen ? "true" : "false"}
               >
