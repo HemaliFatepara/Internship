4e58b0864cc277592d3c86c81712520ec9b3e582
vismay25 <vishmaypatel2573@gmail.com>
2024-02-21 18:36:02 +0530

    .

---
+++ Week-8/task/src/Components/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Wrapper from "./Wrapper";
+import Slider from "./Slider";
+const Home = () => {
+  return (
+    <Fragment>
+      <Slider />
+      <Wrapper />
+    </Fragment>
+  );
+};
+
+export default Home;
